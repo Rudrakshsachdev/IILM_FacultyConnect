227a23fa138a76f6df899358f225633558ca3205
Rudraksh sachdev <rudrakshsachdeva04@gmail.com>
2025-10-31 18:43:35 +0530

    ready for render deployment

--- Faculty_Portal/Faculty_Portal/settings.py
+++ Faculty_Portal/Faculty_Portal/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
-        ssl_require=False  # Change to True when using PostgreSQL on Render
+        ssl_require=True  # Change to True when using PostgreSQL on Render
     )
 }
 # Password validation
